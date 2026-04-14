b3abd5d34713933a07282a7eb082a3b09bd29c39
aliciabuzeli <aliciabuzeli@gmail.com>
2026-04-14 11:24:05 -0300

    Add files via upload

---
+++ EdicaoDoador.jsx
@@ -0,0 +1,9 @@
+import EdicaoDoador1 from "../components/EdicaoDoador1/EdicaoDoador1.jsx";
+
+export default function EdicaoDoador() {
+    return (
+        <>
+            <EdicaoDoador1/>
+        </>
+    )
+}
